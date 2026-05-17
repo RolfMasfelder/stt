ae18a7b4f711fb736202bb903a77a2ff88f3aed0
Rolf Masfelder <rolf@masfelder.de>
2026-05-17 13:29:05 +0200

    mixup

--- src/stt/api/oauth2_validators.py
+++ src/stt/api/oauth2_validators.py
@@ -1,7 +1,10 @@
 """Custom OAuth2 validator to assign user to client credentials tokens."""
 
+import logging
+
 from oauth2_provider.oauth2_validators import OAuth2Validator
 
+logger = logging.getLogger(__name__)
 
 class CustomOAuth2Validator(OAuth2Validator):
     """Assign the application's user to client credentials tokens.
@@ -12,11 +15,20 @@ class CustomOAuth2Validator(OAuth2Validator):
     """
 
     def _save_bearer_token(self, token, request, *args, **kwargs):
+        logger.warning(
+            "CustomOAuth2Validator._save_bearer_token called, grant_type=%s",
+            request.grant_type,
+        )
         super()._save_bearer_token(token, request, *args, **kwargs)
         if request.grant_type == "client_credentials" and request.client.user:
             from oauth2_provider.models import AccessToken
 
-            AccessToken.objects.filter(
+            updated = AccessToken.objects.filter(
                 token=token["access_token"],
                 user__isnull=True,
             ).update(user=request.client.user)
+            logger.warning(
+                "CustomOAuth2Validator: updated %d tokens for user %s",
+                updated,
+                request.client.user,
+            )
