9a1d094ec54bb746a151815c45b7426ec21a0ed7
Rolf Masfelder <rolf@masfelder.de>
2026-05-17 13:29:05 +0200

    mixup

--- src/stt/api/oauth2_validators.py
+++ src/stt/api/oauth2_validators.py
@@ -6,7 +6,6 @@ from oauth2_provider.oauth2_validators import OAuth2Validator
 
 logger = logging.getLogger(__name__)
 
-
 class CustomOAuth2Validator(OAuth2Validator):
     """Assign the application's user to client credentials tokens.
 
